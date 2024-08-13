154ecefc444c018120102c61cc37911ac2adcba5
victoriaparraf <victoriaparrafranco@gmail.com>
2024-08-13 19:32:35 -0400

    04-funciones

--- src/clean-code/04-functions.ts
+++ src/clean-code/04-functions.ts
@@ -29,9 +29,68 @@
     //Limitar los parametros a 3 parametros posicionales 
 
     //Sirve
-    function mandarEmail({ toWhom, from, body, subject, apikey }: sendEmailOptions ): boolean{
+    // function mandarEmail({ toWhom, from, body, subject, apikey }: sendEmailOptions ): boolean{
 
-        return true;
-    }
+    //     return true;
+    // }
+
+        // función para obtener información de una película por Id
+        function getMovieById( id: string ) {
+            console.log({ id });
+        }
+    
+        // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+        function getMovieCast( id: string ) {
+            console.log({ id });
+        }
+    
+        // funcion para obtener el bio del actor por el id
+        function getActorBioById( id: string ) {
+            console.log({ id });
+        }
+        
+        // Crear una película
+        interface Movie{
+            cast: string[];
+            description: string;
+            rating: number;
+            title: string;
+        }
+
+        function createMovie({title, description, rating, cast}: Movie ) {
+            console.log({ title, description, rating, cast });
+        }
+    
+        // Crea un nuevo actor
+        function createActor( fullName: string, birthdate: Date ): boolean {
+            
+            // tarea asincrona para verificar nombre
+            // ..
+            // ..
+            if ( fullName === 'fernando' ) return false;
+    
+            console.log('Crear actor',birthdate);
+            return true;        
+    
+        }
+        //Continuacion
+        const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+            let result;
+            if ( isDead ) {
+                result = 1500;
+            } else {
+                if ( isSeparated ) {
+                    result = 2500;
+                } else {
+                    if ( isRetired ) {
+                        result = 3000;
+                    } else {
+                        result = 4000; 
+                    }
+                }
+            }
+            
+            return result;
+        }
 
 })();
