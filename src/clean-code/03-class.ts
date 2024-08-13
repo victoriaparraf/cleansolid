662f4fe7fe75cc575f89bf8b0e1492585220cb64
victoriaparraf <victoriaparrafranco@gmail.com>
2024-08-13 19:05:51 -0400

    03-clases

---
+++ src/clean-code/03-class.ts
@@ -0,0 +1,23 @@
+(() => {
+    //Malos
+    class Manager{};
+    class Data{};
+    class Info{};
+    class Individual{};
+    class Procesor{};
+    class SpecialMonsterView{};
+    class SpecialViewingCaseMonsterManaegerEventsHandlerActivitySingleton{};
+
+    //El nombre de una clase es importante ya que por el mismo esta recibe muchas responsabilidades
+    //Formados por un sustantivo o frases de sustantivos
+    //No deben ser muy genericos
+    //Usar UpperCamelCase
+
+    //Que exactamente hace la clase
+    //Como exactamente esta clase realiza cierta tarea
+    //Hay algo especifico en esta ubiacion y como se usa esta clase?
+
+    //Si algo no tiene sentido, remuevelo o refactoriza
+    
+
+})();
